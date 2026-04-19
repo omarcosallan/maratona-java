5eda263743473a188f3e517ae3451c2d98ec77db
Marcos <marcosallanm1to@gmail.com>
2026-04-19 18:54:14 -0300

    mj-12: Strings

--- src/academy/devdojo/maratonajava/introducao/Aula02TiposPrimitivos.java
+++ src/academy/devdojo/maratonajava/introducao/Aula02TiposPrimitivos.java
@@ -12,10 +12,12 @@ public class Aula02TiposPrimitivos {
         boolean isTrue = true;
         boolean isFalse = false;
         char character = 65;
+        String nome = "Goku";
 
         System.out.println("A idade é: " + age);
         System.out.println(isTrue);
         System.out.println(character);
         System.out.println(bigNumber);
+        System.out.println("Oi meu nome é " + nome);
     }
 }
