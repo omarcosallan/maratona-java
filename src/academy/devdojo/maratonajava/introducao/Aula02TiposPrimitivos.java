20fd6744ddf20aa71305f2567d5143001b02e5d5
Marcos <marcosallanm1to@gmail.com>
2026-04-19 15:56:14 -0300

    mj-11: tipos primitivos

--- src/academy/devdojo/maratonajava/introducao/Aula02TiposPrimitivos.java
+++ src/academy/devdojo/maratonajava/introducao/Aula02TiposPrimitivos.java
@@ -4,6 +4,16 @@ public class Aula02TiposPrimitivos {
     public static void main(String[] args) {
         // int, double, float, char, byte, short, long, boolean
         int age = 10;
+        long bigNumber = 100000;
+        double salaryD = 2000;
+        float salaryF = 2500;
+        byte ageB = 127;
+        short ageS = 10;
+        boolean isTrue = true;
+        boolean isFalse = false;
+        char character = 65;
         System.out.println("A idade é: " + age);
+        System.out.println(isTrue);
+        System.out.println(character);
     }
 }
