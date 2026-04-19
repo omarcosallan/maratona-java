78dbaba5ef8d86d066cca3ee64826a2b6f99231f
Marcos <marcosallanm1to@gmail.com>
2026-04-19 18:39:33 -0300

    mj-12: tipos primitivos (casting)

--- src/academy/devdojo/maratonajava/introducao/Aula02TiposPrimitivos.java
+++ src/academy/devdojo/maratonajava/introducao/Aula02TiposPrimitivos.java
@@ -3,17 +3,19 @@ package academy.devdojo.maratonajava.introducao;
 public class Aula02TiposPrimitivos {
     public static void main(String[] args) {
         // int, double, float, char, byte, short, long, boolean
-        int age = 10;
-        long bigNumber = 100000;
-        double salaryD = 2000;
-        float salaryF = 2500;
+        int age = (int) 10000000000000L;
+        long bigNumber = (long) 155.23;
+        double salaryD = 2000.0;
+        float salaryF = (float) 2500.0D;
         byte ageB = 127;
         short ageS = 10;
         boolean isTrue = true;
         boolean isFalse = false;
         char character = 65;
+
         System.out.println("A idade é: " + age);
         System.out.println(isTrue);
         System.out.println(character);
+        System.out.println(bigNumber);
     }
 }
