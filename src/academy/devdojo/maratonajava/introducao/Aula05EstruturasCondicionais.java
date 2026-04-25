3e7567af6c95b5a9577ff0b28fdc3a68ef91d8fb
Marcos <marcosallanm1to@gmail.com>
2026-04-25 10:57:05 -0300

    mj-20: Estruturas condicionais (if)

---
+++ src/academy/devdojo/maratonajava/introducao/Aula05EstruturasCondicionais.java
@@ -0,0 +1,16 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula05EstruturasCondicionais {
+    public static void main(String[] args) {
+        int age = 15;
+        boolean isAuthorizedToBuyAlcoholicBeverages = age >= 18;
+        if (isAuthorizedToBuyAlcoholicBeverages) { // Valor boolean sempre true para ser executado
+            System.out.println("Authorized to buy alcoholic beverages");
+        }
+        // !
+        if (!isAuthorizedToBuyAlcoholicBeverages) {
+            System.out.println("Not authorized to buy alcoholic beverages");
+        }
+        System.out.println("Out the if scope");
+    }
+}
