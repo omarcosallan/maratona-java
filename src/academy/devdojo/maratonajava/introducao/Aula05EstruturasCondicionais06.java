43458a987bb864aaec0e7a1324e672e374ee0cbf
Marcos <marcosallanm1to@gmail.com>
2026-04-27 19:47:46 -0300

    mj-26: Estruturas condicionais (switch)

---
+++ src/academy/devdojo/maratonajava/introducao/Aula05EstruturasCondicionais06.java
@@ -0,0 +1,19 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula05EstruturasCondicionais06 {
+    public static void main(String[] args) {
+        byte day = 4;
+        switch (day) {
+            case 1:
+            case 7:
+                System.out.println("Final de semana");
+                break;
+            case 2: case 3: case 4: case 5: case 6:
+                System.out.println("Dia útil");
+                break;
+            default:
+                System.out.println("Inválido");
+                break;
+        }
+    }
+}
